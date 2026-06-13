686d6e7707f7c57fa58e62f9c9386da87b5b5507
Teo <obteo@live.com>
2026-06-13 23:55:48 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 cd /home/container
 
-# Output PHP version (debug utile)
 php -v
 
-# ----------------------------------
-# Replace Startup Variables (Pterodactyl standard)
-# ----------------------------------
 MODIFIED_STARTUP=$(eval echo "$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')")
 
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# ----------------------------------
-# Fix runtime dirs (NO permissions required hacks)
-# ----------------------------------
 mkdir -p /tmp/apache
 
 export APACHE_RUN_DIR=/tmp
 export TMPDIR=/tmp
 
-# ----------------------------------
-# Run the Server
-# ----------------------------------
 exec ${MODIFIED_STARTUP}
