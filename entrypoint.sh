e98d45923630693b4c75b75e15f630f08d5ec8d1
Teo <obteo@live.com>
2026-06-13 23:46:27 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-echo "[XLRSTATS] Starting Apache..."
+# pulizia runtime
+rm -rf /run/httpd/*
 
+# assicurati directory container
 mkdir -p /home/container/public_html
+chown -R apache:apache /home/container
 
-exec /usr/sbin/httpd -D FOREGROUND
+# usa /tmp invece di /run (CRITICO)
+export APACHE_RUN_DIR=/tmp
+export PIDFILE=/tmp/httpd.pid
+
+# avvio apache con config sicura
+exec httpd -DFOREGROUND -c "PidFile /tmp/httpd.pid"
