dbb9b1d53f6f44c5e740245a1ba2881f46a4b3f3
Teo <obteo@live.com>
2026-06-14 00:02:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 cd /home/container
 
+# Debug (opzionale)
 php -v
 
+# Replace Pterodactyl variables
 MODIFIED_STARTUP=$(eval echo "$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')")
 
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
+# Runtime safe dirs
 mkdir -p /tmp/apache
 
 export APACHE_RUN_DIR=/tmp
 export TMPDIR=/tmp
 
+# IMPORTANT: keep process alive
 exec ${MODIFIED_STARTUP}
